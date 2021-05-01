34a13e6240b241f0ecf269e02515cd5c699f4eda
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-05-02 00:35:51 +0200

    Set dataSource layer

--- app/src/main/java/com/example/githubdemo/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/githubdemo/repository/UserRepositoryImpl.kt
@@ -1,4 +1,15 @@
 package com.example.githubdemo.repository
 
-class UserRepositoryImpl {
+import com.example.githubdemo.api.RetrofitBuilder
+import com.example.githubdemo.users.model.UserResponse
+import javax.sql.DataSource
+
+class UserRepositoryImpl : UserRepository {
+
+  private val userDataSource: UserDataSource = UserDataSource()
+
+    override suspend fun getUsers(): List<UserResponse> {
+        return userDataSource.getUsers()
+    }
+
 }
