42f4beb07770b3ab6a4bb42652009328eda57dde
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-05-04 07:16:11 +0200

    Setup UserFragment.kt
    *setup recycler view with the adapter
    *setup view model
    *observe user list, status

--- app/src/main/java/com/example/githubdemo/users/UserFragment.kt
+++ app/src/main/java/com/example/githubdemo/users/UserFragment.kt
@@ -6,19 +6,74 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.githubdemo.R
+import com.example.githubdemo.adapter.UserAdapter
+import com.example.githubdemo.api.UserApiStatus
 import com.example.githubdemo.databinding.FragmentUserBinding
+import com.example.githubdemo.repository.UserRepositoryImpl
 
 
 class UserFragment : Fragment() {
 
+    lateinit var binding: FragmentUserBinding
+    lateinit var userAdapter: UserAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentUserBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_user,container,false)
-        
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_user, container, false)
+        setupRecyclerView()
+
+        val repository = UserRepositoryImpl()
+        val viewModelFactory = UserViewModelProviderFactory(repository)
+        val userViewModel = ViewModelProvider(this, viewModelFactory).get(UserViewModel::class.java)
+
+        userViewModel.users.observe(viewLifecycleOwner, Observer { usersList ->
+            usersList.let {
+                userAdapter.submitList(usersList)
+            }
+        })
+        //for status
+        userViewModel.status.observe(viewLifecycleOwner, Observer { status ->
+            setStatus(binding, status)
+        })
+
+
         return binding.root
+    }
+
+
+    private fun setupRecyclerView() {
+        userAdapter = UserAdapter()
+        binding.apply {
+            userRecycler.adapter = userAdapter
+
+        }
+    }
+
+    private fun setStatus(binding: FragmentUserBinding, status: UserApiStatus) {
+        binding.apply {
+            status?.let {
+                when (status) {
+                    UserApiStatus.LOADING -> {
+                        ivStatus.visibility = View.VISIBLE
+                        ivStatus.setImageResource(R.drawable.loading_animation)
+                    }
+                    UserApiStatus.ERROR -> {
+                        ivStatus.visibility = View.VISIBLE
+                        ivStatus.setImageResource(R.drawable.ic_connection_error)
+                    }
+                    UserApiStatus.DONE -> {
+                        ivStatus.visibility = View.GONE
+                    }
+                }
+            }
+        }
     }
 
 }
