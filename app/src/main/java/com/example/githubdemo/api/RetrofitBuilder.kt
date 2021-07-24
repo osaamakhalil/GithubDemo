c0ce20a45130d399771cf82ca3e47deb5aaf919f
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-07-24 16:57:11 +0200

    Add loggingInterceptor

--- app/src/main/java/com/example/githubdemo/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/githubdemo/api/RetrofitBuilder.kt
@@ -1,15 +1,26 @@
 package com.example.githubdemo.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
     private const val BASE_URL = "https://api.github.com/"
 
+    private val loggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
     private val retrofitBuilder: Retrofit.Builder by lazy {
+
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
     }
     val userApiService: UserApiService by lazy {
         retrofitBuilder
