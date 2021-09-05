3949b4425a65dab302a585e5fa47ead5821de88b
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-09-05 15:27:27 +0200

    Change splash screen logic
     *remove delay way
     *make splash screen background as the activity’s theme background.

--- app/src/main/java/com/example/githubdemo/SplashScreen.kt
+++ app/src/main/java/com/example/githubdemo/SplashScreen.kt
@@ -1,37 +1,15 @@
 package com.example.githubdemo
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import com.example.githubdemo.databinding.FragmentSplashScreenBinding
-import kotlinx.coroutines.delay
 
-
-class SplashScreen : Fragment() {
-
-    private var _binding: FragmentSplashScreenBinding? = null
-    private val binding get() = _binding!!
-
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launchWhenStarted {
-            delay(2000)
-            findNavController().navigate(R.id.toUserFragment2)
-        }
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
 }
