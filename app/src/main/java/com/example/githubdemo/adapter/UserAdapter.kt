061a9f7945ac371fffdb37c2a953dc7ca0027981
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-09-16 16:20:04 +0200

    Add new view holder for paging progress
     *use AsyncListDiffer

--- app/src/main/java/com/example/githubdemo/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/githubdemo/adapter/UserAdapter.kt
@@ -2,28 +2,60 @@ package com.example.githubdemo.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.githubdemo.databinding.ItemUserBinding
+import com.example.githubdemo.databinding.PageProgressBarBinding
 import com.example.githubdemo.users.model.UserResponse
+import com.example.githubdemo.utils.Constant.Companion.USER_LIST_VIEW
+import com.example.githubdemo.utils.Constant.Companion.VIEW_TYPE_LOADING
 
-class UserAdapter : ListAdapter<UserResponse, UserAdapter.ViewHolder>(UserDiffCallBack()) {
 
+class UserAdapter() : ListAdapter<UserResponse, RecyclerView.ViewHolder>(differCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding: ItemUserBinding =
-            ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == USER_LIST_VIEW) {
+            val binding: ItemUserBinding =
+                ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return UserViewHolder(binding)
+        } else {
+            val binding: PageProgressBarBinding =
+                PageProgressBarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return LoadingViewHolder(binding)
+        }
+
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        if (viewHolder is UserViewHolder) {
+            populateItemRows(viewHolder, position)
+        } else if (viewHolder is LoadingViewHolder) {
+            showLoadingView(viewHolder, position)
+        }
     }
 
-    class ViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == differ.currentList.size - 1)
+            VIEW_TYPE_LOADING
+        else
+            USER_LIST_VIEW
+    }
+
+    override fun submitList(list: List<UserResponse>?) {
+        super.submitList(list?.let { ArrayList(it) })
+    }
+
+    class UserViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(user: UserResponse) {
             binding.apply {
                 tvUserName.text = user.name
@@ -34,8 +66,31 @@ class UserAdapter : ListAdapter<UserResponse, UserAdapter.ViewHolder>(UserDiffCa
             }
         }
     }
+
+    class LoadingViewHolder(private val binding: PageProgressBarBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.pageProgress
+        }
+    }
+
+    private fun showLoadingView(viewHolder: LoadingViewHolder, position: Int) {}
+
+
+    private fun populateItemRows(viewHolder: UserViewHolder, position: Int) {
+        val item = differ.currentList[position]
+        viewHolder.bind(item)
+    }
+
 }
-class UserDiffCallBack : DiffUtil.ItemCallback<UserResponse>() {
+
+/**
+ * Callback for calculating the diff between two non-null items in a list.
+ *
+ * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * list that's been passed to `submitList`.
+ */
+private val differCallback = object : DiffUtil.ItemCallback<UserResponse>() {
     override fun areItemsTheSame(oldItem: UserResponse, newItem: UserResponse): Boolean {
         return oldItem.id == newItem.id
     }
