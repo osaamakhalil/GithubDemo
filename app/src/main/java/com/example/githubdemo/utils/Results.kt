4e1c19a85eb267628605120abba77bec753d0eae
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-09-22 21:35:20 +0200

    Enhance Results.kt
     *change loading from class to object
     *add new object "no internet"
     *add new bool on success class is error next

--- app/src/main/java/com/example/githubdemo/utils/Results.kt
+++ app/src/main/java/com/example/githubdemo/utils/Results.kt
@@ -1,12 +1,14 @@
 package com.example.githubdemo.utils
 
-sealed class Results<T>(
+
+sealed class Results<out T>(
     val data: T? = null,
     val message: String? = null
 
 ) {
-    class Loading<T> : Results<T>()
-    class Success<T>(data: T?) : Results<T>(data)
-    class Error<T>(message: String? = null, data: T? = null) : Results<T>(data, message)
+    object Loading : Results<Nothing>()
+    object NoInternet: Results<Nothing>()
+    class Success<out T>(data: T?, val isErrorNext: Boolean) : Results<T>(data)
+    class Error<out T>(message: String? = null, data: T? = null) : Results<T>(data, message)
 }
 
