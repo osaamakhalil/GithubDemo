1ead3c3b7413d966f61000850978031843f31b53
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-09-22 21:43:36 +0200

    Separate user view holder from user adapter

--- app/src/main/java/com/example/githubdemo/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/githubdemo/adapter/UserAdapter.kt
@@ -1,25 +1,23 @@
 package com.example.githubdemo.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.githubdemo.databinding.ItemUserBinding
-import com.example.githubdemo.databinding.PageProgressBarBinding
-import com.example.githubdemo.users.UserFragment
+import com.example.githubdemo.databinding.NetworkStatusBinding
+import com.example.githubdemo.users.*
 import com.example.githubdemo.users.model.UserResponse
 import com.example.githubdemo.utils.Constant.Companion.USER_LIST_VIEW
 import com.example.githubdemo.utils.Constant.Companion.VIEW_TYPE_LOADING
-import com.example.githubdemo.viewholder.LoadingViewHolder
+import com.example.githubdemo.viewholder.NetworkStatusViewHolder
+import com.example.githubdemo.viewholder.UserViewHolder
 import com.example.githubdemo.viewholder.showLoadingView
 
 
-class UserAdapter(val userFragment: UserFragment) : ListAdapter<UserResponse, RecyclerView.ViewHolder>(UserDiffCallBack()) {
-
+class UserAdapter(val userFragment: UserFragment, private val onTryAgainClick: () -> Unit) :
+    ListAdapter<UserResponse, RecyclerView.ViewHolder>(UserDiffCallBack()) {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -27,10 +25,10 @@ class UserAdapter(val userFragment: UserFragment) : ListAdapter<UserResponse, Re
             val binding: ItemUserBinding =
                 ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return UserViewHolder(binding)
-        } else  {
-            val pageProgressBarBinding: PageProgressBarBinding =
-                PageProgressBarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return LoadingViewHolder(pageProgressBarBinding,userFragment)
+        } else {
+            val pageProgressBarBinding: NetworkStatusBinding =
+                NetworkStatusBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return NetworkStatusViewHolder(pageProgressBarBinding, onTryAgainClick)
         }
 
     }
@@ -38,8 +36,8 @@ class UserAdapter(val userFragment: UserFragment) : ListAdapter<UserResponse, Re
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
         if (viewHolder is UserViewHolder) {
             populateItemRows(viewHolder, position)
-        } else if (viewHolder is LoadingViewHolder) {
-            showLoadingView(viewHolder, position,userFragment)
+        } else if (viewHolder is NetworkStatusViewHolder) {
+            showLoadingView(viewHolder, position, userFragment)
         }
     }
 
@@ -53,21 +51,6 @@ class UserAdapter(val userFragment: UserFragment) : ListAdapter<UserResponse, Re
         else
             USER_LIST_VIEW
     }
-
-
-    class UserViewHolder(private val binding: ItemUserBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(user: UserResponse) {
-            binding.apply {
-                tvUserName.text = user.name
-                Glide.with(itemView.context)
-                    .load(user.imageUrl)
-                    .circleCrop()
-                    .into(ivUserAvatar)
-            }
-        }
-    }
-
 
     private fun populateItemRows(viewHolder: UserViewHolder, position: Int) {
         val item = getItem(position)
---
+++ app/src/main/java/com/example/githubdemo/viewholder/UserViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.githubdemo.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.githubdemo.databinding.ItemUserBinding
+import com.example.githubdemo.users.model.UserResponse
+
+class UserViewHolder(private val binding: ItemUserBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(user: UserResponse) {
+        binding.apply {
+            tvUserName.text = user.name
+            Glide.with(itemView.context)
+                .load(user.imageUrl)
+                .circleCrop()
+                .into(ivUserAvatar)
+        }
+    }
+}
+
