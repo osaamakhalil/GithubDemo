16f3be5770de7883daf2dca25721384f4738cf0e
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-10-12 22:39:56 +0200

    Add 2 new items to repo_item.xml

--- app/src/main/java/com/example/githubdemo/users/model/UserRepo.kt
+++ app/src/main/java/com/example/githubdemo/users/model/UserRepo.kt
@@ -5,5 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class UserRepo(
     @SerializedName("name")
     val repoName: String,
-) {
+    @SerializedName("stargazers_count")
+    val starsCount: Int,
+    @SerializedName("forks_count")
+    val forksCount: Int,
+
+    ) {
 }
--- app/src/main/java/com/example/githubdemo/viewholder/RepoViewHolder.kt
+++ app/src/main/java/com/example/githubdemo/viewholder/RepoViewHolder.kt
@@ -7,6 +7,11 @@ import com.example.githubdemo.users.model.UserRepo
 class RepoViewHolder(private val binding: RepoItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(userRepo: UserRepo) {
-        binding.tvUserRepo.text = userRepo.repoName
+        binding.apply {
+            tvUserRepo.text = userRepo.repoName
+            tvStarsCount.text = userRepo.starsCount.toString()
+            tvForkCount.text = userRepo.forksCount.toString()
+        }
+
     }
 }
