2be58245a42edf28da99df75d8f82a57ecf4922e
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-10-14 07:28:36 +0200

    Finish Home Ui
     *make implicit intent to open wep page

---
+++ app/src/main/java/com/example/githubdemo/users/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.example.githubdemo.users.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.githubdemo.R
+import com.example.githubdemo.databinding.FragmentHomeBinding
+import android.content.Intent
+import android.net.Uri
+
+
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+            _binding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigateToUsersFragment()
+        openGithubWepPage()
+    }
+
+
+    private fun navigateToUsersFragment() {
+        binding.tvUsers.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.fromHomeFragmentToUserFragment())
+        }
+    }
+
+    private fun openGithubWepPage() {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com"))
+        binding.tvGitHubWep.setOnClickListener {
+            startActivity(browserIntent)
+        }
+    }
+
+}
