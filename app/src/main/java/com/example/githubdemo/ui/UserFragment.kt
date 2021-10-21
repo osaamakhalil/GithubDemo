7033eae7aeed5c2712b18508613b8b871c16a1c2
osama abdelfatah khalil <59806410+osaamakhalil@users.noreply.github.com>
2021-10-21 02:11:01 +0200

    Finish search
     *handle paging
     *handle error cases
     *hide keyboard

--- app/src/main/java/com/example/githubdemo/ui/UserFragment.kt
+++ app/src/main/java/com/example/githubdemo/ui/UserFragment.kt
@@ -1,15 +1,20 @@
 package com.example.githubdemo.ui
 
+import android.app.Activity
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AbsListView
+import android.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.coroutineScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,11 +24,15 @@ import com.example.githubdemo.utils.NetworkUtil
 import com.example.githubdemo.databinding.FragmentUserBinding
 import com.example.githubdemo.repository.UserRepositoryImpl
 import com.example.githubdemo.db.UsersDatabase
-import com.example.githubdemo.users.home.UserViewModel
-import com.example.githubdemo.users.home.UserViewModelProviderFactory
+import com.example.githubdemo.users.Users.UserViewModel
+import com.example.githubdemo.users.Users.UserViewModelProviderFactory
 import com.example.githubdemo.users.model.UserResponse
 import com.example.githubdemo.utils.Results
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.view.inputmethod.InputMethodManager
 
 
 class UserFragment : Fragment() {
@@ -34,6 +43,9 @@ class UserFragment : Fragment() {
     val networkUtil: NetworkUtil by lazy {
         NetworkUtil(requireContext())
     }
+    var searchText = ""
+    var isSearchList = false
+    private lateinit var cashUsersList: List<UserResponse>
 
     private val userViewModel: UserViewModel by viewModels {
         val repository =
@@ -55,15 +67,17 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        userListResultsHandling()
+        userListResults()
         snackBarView(view)
         setupRecyclerView(networkUtil)
         swipeRefresh()
         tryAgainButton()
         navigateToBack()
+        searchForUser()
+        userSearchListResults()
     }
 
-    private fun userListResultsHandling() {
+    private fun userListResults() {
         userViewModel.usersStatus.observe(viewLifecycleOwner, { usersResults ->
             usersResults?.let { response ->
                 when (response) {
@@ -73,6 +87,8 @@ class UserFragment : Fragment() {
                         serverErrorView(false)
                         noInternetView(false)
                         response.data?.let { userList ->
+                            isSearchList = false
+                            cashUsersList = userList
                             listUserAdapter.submitList(userList)
                             listUserAdapter.notifyItemChanged(userList.lastIndex)
                         }
@@ -104,12 +120,87 @@ class UserFragment : Fragment() {
         })
     }
 
+    private fun userSearchListResults() {
+        userViewModel.searchStatus.observe(viewLifecycleOwner, { usersResults ->
+            usersResults?.let { response ->
+                when (response) {
+                    is Results.Success -> {
+                        tryAgainView(false)
+                        progressbarView(false)
+                        serverErrorView(false)
+                        noInternetView(false)
+                        response.data?.let { userList ->
+                            isSearchList = true
+                            listUserAdapter.submitList(response.data)
+                            listUserAdapter.notifyItemChanged(response.data.lastIndex)
+                        }
+                    }
+                    Results.Loading -> {
+                    }
+                    is Results.Error -> {
+                        listUserAdapter.submitList(emptyList())
+                        tryAgainView(true)
+                        progressbarView(false)
+                        noInternetView(false)
+                        serverErrorView(true)
+                        response.message?.let { message ->
+                            Log.e("UserFragment", "An error occured: $message")
+                        }
+                    }
+                    Results.NoInternet -> {
+                    }
+                }
+            }
+        })
+    }
+
     private fun snackBarView(view: View) {
         userViewModel.showSnackBar.observe(viewLifecycleOwner, { showSnackBar ->
             if (showSnackBar == true) {
                 Snackbar.make(view, "NO INTERNET CONNECTION !!", Snackbar.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun searchForUser() {
+        binding.userSearchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                var debouncePeriod: Long = 500
+                private val coroutineScope = lifecycle.coroutineScope
+                private var searchJob: Job? = null
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    searchJob?.cancel()
+                    searchJob = coroutineScope.launch {
+                        newText?.let {
+                            delay(debouncePeriod)
+                            if (it.isEmpty()) {
+                                listUserAdapter.submitList(cashUsersList)
+                                isSearchList = false
+                                closeKeyboard()
+                            } else {
+                                userViewModel.clearSearch()
+                                userViewModel.searchForUsers(it)
+                                searchText = it
+                            }
+                        }
+                    }
+                    return false
+                }
+            }
+        )
+    }
+
+     private fun closeKeyboard() {
+        val view: View? = requireActivity().currentFocus
+        if (view != null) {
+            val imm = activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+            imm!!.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
     private fun setupRecyclerView(networkUtil: NetworkUtil) {
@@ -130,6 +221,7 @@ class UserFragment : Fragment() {
     }
 
     private fun navigateToDetailsScreen(user: UserResponse) {
+        closeKeyboard()
         findNavController().navigate(UserFragmentDirections.toUserDetails(user))
     }
 
@@ -150,8 +242,13 @@ class UserFragment : Fragment() {
             val isNotAtBeginning = firstVisibleItemPosition >= 0
             val shouldPaginate = isAtLastItem && isNotAtBeginning && isScrolling
             if (shouldPaginate) {
-                userViewModel.getUsers()
-                isScrolling = false
+                if (isSearchList) {
+                    userViewModel.searchForUsers(searchText)
+                    isScrolling = false
+                } else {
+                    userViewModel.getUsers()
+                    isScrolling = false
+                }
             }
         }
 
